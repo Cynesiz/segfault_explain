b1c32980fe65f1582b0fcf2c5cc3175412220207
shekk <buttsecz@gmail.com>
2015-06-16 11:30:58 -0500

    Update segfault_catch.sh

--- segfault_catch.sh
+++ segfault_catch.sh
@@ -26,3 +26,4 @@ x=1; while [ $x = 1 ];
 gdb -return-child-result -ex run -ex "thread apply all bt" -ex "quit" $2 -p $pid
 #while [ $x = 1 ]; do process=$(pgrep -u $user); if [ $pid ]; then x=0; fi;  done; strace -vvttf -s 256  -p $pid 2>&1| tee ./$LOCKING.strace
 #grep rw-p /proc/$pid/maps | sed -n 's/^\([0-9a-f]*\)-\([0-9a-f]*\) .*$/\1 \2/p' | while read start stop; do gdb --batch --pid $pid -ex "dump memory $1-$start-$stop.dump 0x$start 0x$stop"; done
+#tail -32768 /var/log/messages|grep segfault|while read ; do echo -e "\n$REPLY" ; echo $REPLY|grep -oP '(?<=ip )[0-9a-f]+(?= sp)'|sort|uniq|xargs -I{} gdb /usr/bin/php --batch --ex 'x/1wx 0x{}' ;done
